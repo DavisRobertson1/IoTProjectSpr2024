6f311c1b3bb67c2b37eca18164dec0345ea6dec3
DavisRobertson1 <67032827+DavisRobertson1@users.noreply.github.com>
2024-06-05 17:55:08 -0700

    debugging

--- finalNoLoop.py
+++ finalNoLoop.py
@@ -80,7 +80,7 @@ i2c = busio.I2C(board.SCL, board.SDA)
 ads = ADS.ADS1115(i2c)
 # Create a single ended input on each channel 0
 possibleChannelInputs = [ADS.P0, ADS.P1, ADS.P2, ADS.P3]
-plants[i].channel = AnalogIn(ads, possibleChannelInputs[i] for i in range(0, len(plants)))
+plants[i].channel = AnalogIn(ads, possibleChannelInputs[i]) for i in range(0, len(plants))
 #MAIN CODE
 #collect samples and find average
 for plant in plants:
